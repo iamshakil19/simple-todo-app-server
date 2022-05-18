5e892c2eee5ab0d0bbdc99e53d1cc79bcbb94fe7
shakilahmed09 <shakilahmed.pure@gmail.com>
2022-05-19 00:25:57 +0600

    todo get and post api created

--- index.js
+++ index.js
@@ -3,11 +3,41 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 app.use(cors());
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cq469.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect()
+
+        const todoCollection = client.db('simple-todo').collection('todo')
+
+        app.get('/todo', async (req, res) => {
+            const allTodo = await todoCollection.find().toArray()
+            res.send(allTodo)
+        })
+
+        app.post('/todo', async (req, res) => {
+            const todo = req.body;
+            const result = await todoCollection.insertOne(todo)
+            res.send(result)
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
